d11bf8da5a72845a7cbc87cb40802b2642f4a73e
OlegKrivenko <krivenko.olegus@gmail.com>
2023-08-28 19:11:46 +0300

    MoviesGalleryItem coment PropTypes

--- src/components/MoviesGalleryItem/MoviesGalleryItem.jsx
+++ src/components/MoviesGalleryItem/MoviesGalleryItem.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Link, useLocation } from 'react-router-dom';
 import css from './MoviesGalleryItem.module.css';
 
@@ -31,6 +31,6 @@ const MoviesGalleryItem = ({
   );
 };
 
-MoviesGalleryItem.propTypes = {};
+// MoviesGalleryItem.propTypes = {};
 
 export default MoviesGalleryItem;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,41 @@
-// import css from './Reviews.module.css';
+import { useEffect, useState } from 'react';
+import api from '../../services/movies-api';
+import css from './Reviews.module.css';
+import { useParams } from 'react-router-dom';
 
 const Reviews = () => {
-  return <div></div>;
+  const [reviews, setReviews] = useState([]);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    api
+      .getReviewsMovie(movieId)
+      .then(response => {
+        console.log(response.data.results);
+        setReviews(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [movieId]);
+
+  return (
+    <div className={css.reviews__container}>
+      <h2 className={css.tittle}>Reviews</h2>
+      <ul className={css.reviewsBox}>
+        {reviews.length
+          ? reviews.map(({ id, author, content }) => {
+              return (
+                <li className={css.reviewsBox__item} key={id}>
+                  <p className={css.reviewsBox__name}>{author}</p>
+                  <p className={css.reviewsBox__content}>{content}</p>
+                </li>
+              );
+            })
+          : 'Sorry, movie does not have reviews'}
+      </ul>
+    </div>
+  );
 };
 
 export default Reviews;
